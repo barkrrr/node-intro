3ab99a7f9dd6b6ec0dbf54aaf1917d7863101994
Barbara Koehler <barkoehler@gmail.com>
2018-09-11 06:24:26 -0400

    initial commmit

---
+++ main.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const functions = require('./function')
+const chalk = require('chalk');
+
+let counter = 0;
+const intervalId = setInterval(() => {
+  const option = functions.getRandomOption();
+  console.log(chalk.red(option));
+  counter++;
+  if (counter === 10) {
+    clearInterval(intervalId);
+  }
+}, 5000);
+
+console.log(chalk.yellow(functions.options));
